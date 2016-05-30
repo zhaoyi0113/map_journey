314bf79855349d19e1980f5c609029bbd7e44a20
Jian <jianchen@thoughtworks.com>
2016-05-30 11:35:49 +0800

    add control-panel

--- app/src/main/mapjourney/src/components/StationMap/index.js
+++ app/src/main/mapjourney/src/components/StationMap/index.js
@@ -102,8 +102,12 @@ class StationMap extends Component {
                         
                     </Select>
                 </div>
+                
                 <div className='control-panel'>
-                    <Button className=''></Button>
+                    <button> A </button>
+                    <button> B </button>
+                    <button> C </button>
+                    <button> D </button>
                 </div>
             </div>
         )
