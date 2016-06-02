9f767240e639fe4e098f2b25cf97ca6b53095a79
Jian <jianchen@thoughtworks.com>
2016-06-02 17:21:57 +0800

    add navigation action

--- app/src/main/mapjourney/src/components/Navigatior/index.jsx
+++ app/src/main/mapjourney/src/components/Navigatior/index.jsx
@@ -4,15 +4,32 @@ import './navigator.less';
 
 class Navigator extends Component {
 
+  constructor(props, context) {
+    super(props);
+  }
+
+  handleClickNav(type) {
+    if(type==="BACK") {
+      this.context.router.goBack();
+    }
+    if(type==="MAP") {
+      this.context.router.push("/map")
+    }
+  }
+
   render() {
     return(
       <div className="navigator-bar">
-        <div className="left-icon"></div>
+        <div className="left-icon" onClick={this.handleClickNav.bind(this, "BACK")}></div>
         <div className="title">{this.props.title}</div>
-        <div className="right-icon"></div>
+        <div className="right-icon" onClick={this.handleClickNav.bind(this, "MAP")}></div>
       </div>
     )
   }
 }
+
+Navigator.contextTypes = {
+  router: React.PropTypes.object.isRequired
+};
 
 export default Navigator
